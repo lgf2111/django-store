50d8d60c9c04d72031c7a5ae2a7a71ae123ee886
lgf2111 <lgf2111@gmail.com>
2022-08-27 07:12:45 +0000

    t4

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'exhlfdat&vfum(-34*c2uroi(($ww(yo$9pv98=e6p^gl(-eoj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
