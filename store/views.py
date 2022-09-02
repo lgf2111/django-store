7ba9ed50803ab198f4ff15c6268a6caa1fb50b47
lgf2111 <lgf2111@gmail.com>
2022-09-02 19:12:21 +0800

    about not working

--- store/views.py
+++ store/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'seller': 'lgf2111',
+        'title': 'Macbook Pro',
+        'description': 'First ever macbook gotten.',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'seller': 'Jane Doe',
+        'title': 'Samsung TV',
+        'description': 'Last ever tv gotten.',
+        'date_posted': 'August 28, 2018'
+    },
+]
+
 def home(request):
-    return HttpResponse('<h1>Store Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'store/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Store About</h1>')
+    return HttpResponse(request, 'store/about.html')
