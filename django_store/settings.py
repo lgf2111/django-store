47672a90a7bfc7c1b4799afc8a34a8487d202af3
lgf2111 <lgf2111@gmail.com>
2022-09-05 17:42:14 +0800

    update base

--- django_store/settings.py
+++ django_store/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'store.apps.StoreConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
