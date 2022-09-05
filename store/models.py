cf8f25e6f8066e2a3c75af5638e5b2685ac42806
lgf2111 <lgf2111@gmail.com>
2022-09-05 16:02:41 +0800

    migrated product db

--- store/models.py
+++ store/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.FloatField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    shop = models.ForeignKey(User, on_delete=models.CASCADE)
