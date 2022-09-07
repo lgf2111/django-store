1e8dcd4227cf5b306c178788d612090cf1ce2a53
lgf2111 <lgf2111@gmail.com>
2022-09-08 04:10:10 +0800

    update js

--- store/static/store/script/product_detail.js
+++ store/static/store/script/product_detail.js
@@ -1,13 +1,36 @@
-const quantity = document.getElementById('quantity')
+const quantity = document.getElementById("quantity");
 function minusQuantity() {
-    quantity.value > 0 ? quantity.value-- : null
+  quantity.value > 0 ? quantity.value-- : null;
 }
 function plusQuantity() {
-    quantity.value++
+  quantity.value++;
 }
 function checkQuantity() {
-    quantity.value = quantity.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+  quantity.value = quantity.value
+    .replace(/[^0-9.]/g, "")
+    .replace(/(\..*)\./g, "$1");
 }
 function checkQuantityNull() {
-    quantity.value === '' ? quantity.value = 0 : null
+  quantity.value === "" ? (quantity.value = 0) : null;
+}
+
+function playerImg() {
+  let playerImg = document.getElementById("playerimg");
+  if (!playerImg) {
+    playerImg = document.createElement("img");
+    playerImg.id = "playerimg";
+    document.getElementById("playerbox").appendChild(playerImg);
+  }
+  return playerImg;
+}
+
+function setPlayerImg(src) {
+  playerImg().setAttribute("src", src);
+}
+
+const imgs = document.getElementsByClassName("product-image");
+
+for (let i = 0; i < imgs.length; i++) {
+  const el = imgs[i];
+  el.addEventListener("click", () => setPlayerImg(el.src), false);
 }
